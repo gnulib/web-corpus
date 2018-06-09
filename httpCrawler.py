0bdf5c7f0b4bd73c84c731b94e687266d347bf30
Amit Bhadoria <gnulib@gmail.com>
2018-06-08 17:24:04 -0700

    changed crawler implementation to iterative, due to recursion limits on big site crawl

--- httpCrawler.py
+++ httpCrawler.py
@@ -8,10 +8,12 @@ class HttpCrawler():
 	corpus = None
 	baseUrl = None
 	siteBase = None
+	linkList = None
 
 	def __init__(self, baseUrl):
 		self.visited = {}
 		self.corpus = []
+		self.linkList = []
 		self.baseUrl = baseUrl
 		parts=baseUrl.split('/')
 		if len(parts) >= 3:
@@ -35,38 +37,47 @@ class HttpCrawler():
 
 		return True
 
-	def buildCorpus(self, url):
+	def crawl(self, url):
 		if not self.urlInBase(url):
 			return
 		if url in self.visited:
 			self.visited[url] += 1
 			return
-		else:
-			self.visited[url] = 1
-			print 'Crawling: %s ...' % url
+		self.linkList = [url]
+		self.visited[url] = 1
+		self.buildCorpus()
+
+	def buildCorpus(self):
 
 		tknzr = HTMLTokenizer()
 
-		words, links = tknzr.parse(requests.get(url).text)
-		with open(CORPUS_DIR + '/' + url.replace('/', '_'), 'w') as f:
-			for word in words:
-				self.corpus.append(word)
-				f.write(word.encode('utf-8') + ' ')
-				# if word in self.corpus:
-				# 	self.corpus[word] += 1
-				# else:
-				# 	self.corpus[word] = 1
-		for link in links:
-			if len(link) == 0:
-				# skip blank url
-				continue
-			if link[0] == '#':
-				# skip fragment within the page
-				continue
-			# print 'Crawling: %s ...' % link
-			if len(link) < len('http://') or link[:len('http://')] not in ['https:/', 'http://']:
-				# append relative path to site's base url
-				link = self.siteBase + link
-			self.buildCorpus(link)
-
+		while len(self.linkList) > 0:
+			url = self.linkList[0]
+			self.linkList = self.linkList[1:]
+			print 'Crawling: %s ...' % url
+			words, links = tknzr.parse(requests.get(url).text)
+			with open(CORPUS_DIR + '/' + url.replace('/', '_'), 'w') as f:
+				for word in words:
+					# self.corpus.append(word)
+					f.write(word.encode('utf-8') + ' ')
+			for link in links:
+				if len(link) == 0:
+					# skip blank url
+					continue
+				if link[0] == '#':
+					# skip fragment within the page
+					continue
+				# print 'Crawling: %s ...' % link
+				if len(link) < len('http://') or link[:len('http://')] not in ['https:/', 'http://']:
+					# append relative path to site's base url
+					link = self.siteBase + link
+				if not self.urlInBase(link):
+					continue
+				if link in self.visited:
+					self.visited[link] += 1
+					continue
+				else:
+					# print 'Queuing: %s ...' % link
+					self.visited[link] = 1
+					self.linkList.append(link)
 		return
